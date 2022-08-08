ec24b03db8adb4020c137c2dbcc725cb0219673c
kda-data <kda-data@protonmail.com>
2022-08-08 09:05:55 +0800

    modify DefaultDelayConfirmations = 0

--- chains/evm/calls/consts/consts.go
+++ chains/evm/calls/consts/consts.go
@@ -7,5 +7,5 @@ const DefaultDeployGasLimit = 6000000
 const DefaultGasPrice = 20000000000
 const DefaultGasMultiplier = 1
 const DefaultBlockConfirmations = 0
-const DefaultDelayConfirmations = 15
+const DefaultDelayConfirmations = 0
 const DefaultBlockRetryInterval = 5 * time.Second
